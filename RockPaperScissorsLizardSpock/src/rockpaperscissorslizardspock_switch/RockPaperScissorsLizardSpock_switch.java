838f30f8759267539ce664e9915560111bbb9b47
Ishan Madan <ishan@mistlogic.com>
2019-09-11 16:03:33 -0700

    renamed files

--- RockPaperScissorsLizardSpock/src/rockpaperscissorslizardspock_switch/RockPaperScissorsLizardSpock_switch.java
+++ RockPaperScissorsLizardSpock/src/rockpaperscissorslizardspock_switch/RockPaperScissorsLizardSpock_switch.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rockpaperscissors;
+package rockpaperscissorslizardspock_switch;
 
 import java.util.Scanner;
 
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author ishanmadan
  */
-public class RockPaperScissors {
+public class RockPaperScissorsLizardSpock_switch {
 
     /**
      * @param args the command line arguments
